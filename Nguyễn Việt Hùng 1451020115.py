ed91fb80e3c3f88f68e34d275275949843f6da06
hung1451020115 <nguyenviethungqt20@gmail.com>
2021-11-29 20:01:35 +0700

    first commit

---
+++ Nguyễn Việt Hùng 1451020115.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.0.31912.275
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "PythonApplication1", "PythonApplication1.pyproj", "{97F463B1-1649-4AAC-8E04-A24B4C5AA122}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{97F463B1-1649-4AAC-8E04-A24B4C5AA122}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{97F463B1-1649-4AAC-8E04-A24B4C5AA122}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {817040FD-3A4F-42B5-8EC1-E87F8E0DB2EA}
+	EndGlobalSection
+EndGlobal
